25e05dc7ab4b2e8e5ea7ae480aedd2743291533d
1ms17me105 <1ms17me105@gmail.com>
2023-10-19 21:35:02 +0530

    Added some more queries

--- Queries/SQLQuery1.sql
+++ Queries/SQLQuery1.sql
@@ -73,13 +73,49 @@
 --		 population
 --order by population desc;
 
-select
-	location,
-	population,
-	max(total_cases) as maximum_cases,
-	max(total_cases*1.0/population)*100 as percentage_infected
-from project1..covid_deaths
-group by location,
-		 population
-order by percentage_infected desc;
+--select
+--	location,
+--	population,
+--	max(total_cases) as maximum_cases,
+--	max(total_cases*1.0/population)*100 as percentage_infected
+--from project1..covid_deaths
+--group by location,
+--		 population
+--order by percentage_infected desc;
+
+-- ## Countries with highest death counts
+--select
+--	location,
+--	max(total_deaths) as maximum_deaths
+--from project1..covid_deaths
+--where continent is not null -- Only countries
+--group by location
+--order by maximum_deaths desc;
+
+--select location, total_deaths from project1..covid_deaths
+--where location = 'Scotland'; --??
+
+-- ## Countries with highest death count per poulation
+-- To cast one data type to int - cast([column_name] as int)
+--select
+--	location,
+--	population,
+--	max(total_deaths) as maximum_deaths,
+--	max(total_deaths*1.0/population) as maximum_death_rate
+--from project1..covid_deaths
+--group by location,
+--		 population
+--order by maximum_death_rate desc;
+
+-- ## Death rate by continent
+--select
+--	location,
+--	max(total_deaths) as total_deaths
+--from project1..covid_deaths
+--where continent is null
+--group by location
+--having location in ('Europe', 'Asia', 'North America', 'South America', 'Africa', 'Oceania')
+--order by total_deaths desc;
+
+--select * from project1..covid_deaths;
 
